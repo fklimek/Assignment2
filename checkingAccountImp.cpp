fdcbf0cb722970b546c75e4ad8c13fe8931790df
Francis Klimek <fklimek@atu.edu>
2025-01-30 16:06:43 -0600

    Recommitting everything because VScode sucks

---
+++ checkingAccountImp.cpp
@@ -0,0 +1,85 @@
+
+//Implementation file checkingAccountImp.cpp
+  
+#include <iostream>
+#include <iomanip>
+#include "checkingAccount.h"
+
+using namespace std;
+
+checkingAccount::checkingAccount(int acctNumber, double bal, 
+                                 double minBal, double intRate, double servC)
+               : bankAccount(acctNumber, bal)
+               // get the account number and balance from the base class........................ - done
+{
+    interestRate = intRate;
+    minimumBalance = minBal;
+    serviceCharge = servC;
+}
+
+double checkingAccount::getMinimumBalance() const 
+{
+    return minimumBalance;
+}
+
+void checkingAccount::setMinimumBalance(double minBalance)
+{
+    // set the minimum balance with the parameters - done
+    minimumBalance = minBalance;
+}
+
+double checkingAccount::getInterestRate() const
+{
+    return interestRate;
+}
+
+void checkingAccount::setInterestRate(double intRate)
+{
+    interestRate = intRate;
+}
+
+// declare the getter function for service charge - done
+double checkingAccount::getServiceCharge() const
+{
+    return serviceCharge;
+}
+
+void checkingAccount::setServiceCharge(double servC)
+{
+    serviceCharge = servC;
+}
+
+// define the void post interest function - done
+void checkingAccount::postInterest()
+{
+    balance = balance + balance * interestRate;
+}
+
+bool checkingAccount::verifyMinimumumBalance(double amount)
+{
+    return (balance - amount >= minimumBalance);
+}
+
+ void checkingAccount::writeCheck(double amount)
+{
+    withdraw(amount);
+}
+
+void checkingAccount::withdraw(double amount)
+{
+// verify if the balance less than minimum balance - done
+    if (balance < minimumBalance)
+    {
+        cout << "Balance below minimum. Service charge imposed." << endl;
+        bankAccount::withdraw(amount + serviceCharge);
+    }
+    else
+        bankAccount::withdraw(amount);
+}
+
+void checkingAccount::print() const
+{
+    cout << fixed << setprecision(2);
+    cout << "Interest Checking ACCT#:\t" << getAccountNumber()
+         << "\tBalance: $" << getBalance() << endl;
+}
---
+++ savingsAccountImp.cpp
@@ -0,0 +1,48 @@
+
+//Implementation File savingsAccountImp.cpp
+
+#include <iostream>
+#include <iomanip>
+#include "savingsAccount.h"
+  
+using namespace std; 
+
+savingsAccount::savingsAccount(int acctNumber, double bal, 
+                   double intRate)
+       :bankAccount(acctNumber, bal)
+{
+    interestRate = intRate;
+}
+
+double savingsAccount::getInterestRate() const
+{
+    return interestRate; 
+}
+
+void savingsAccount::setInterestRate(double rate)
+{
+    interestRate = rate;
+}
+
+void savingsAccount::withdraw(double amount)
+{
+    if (balance - amount < 0)
+        // write the cout statement
+        cout << "Error: Insufficient funds\n";
+    else 
+        balance = balance - amount;
+}
+
+void savingsAccount::postInterest()
+{
+    balance = balance + balance * interestRate;
+ // equation for post interest
+}
+
+void savingsAccount::print() const
+{
+   // print the account information(account number and balance)
+    cout << fixed << showpoint << setprecision(2);
+    cout << "Interest Savings ACCT#:\t\t" << accountNumber;
+    cout << "\tBalance: $" << balance << endl;
+}
